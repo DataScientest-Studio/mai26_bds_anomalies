30de1c9f5923f6e865b342cb85b7d443a42bc45d
Vincent V <win100@caramail.fr>
2026-07-13 18:05:05 +0200

    centrage de la photo de la vis

--- 2-autoencoder-convolution/preprocessing_screw.py
+++ 2-autoencoder-convolution/preprocessing_screw.py
@@ -22,13 +22,61 @@ def get_screw_contour(image):
 
     return contour
 
-def rotateImage(image, angle):
-    R = cv2.getRotationMatrix2D((image.shape[1]//2, image.shape[0]//2), angle, scale=1)
+def align_horizontally(image):
+    contour = get_screw_contour(image)
+
+    [vx, vy, x0, y0] = cv2.fitLine(contour, cv2.DIST_L2,0,0.01,0.01)
+    angle = np.degrees(np.arctan2(vy, vx))[0]
+
+    image= rotate_image(image,angle)
+
+    return image
+
+def rotate_image(image, angle):
+    center=(image.shape[1]//2, image.shape[0]//2)
+    R = cv2.getRotationMatrix2D(center, angle, scale=1)
+
+    cos = abs(R[0, 0])
+    sin = abs(R[0, 1])
+
+    new_width = int(np.ceil(image.shape[0] * sin + image.shape[1] * cos))
+    new_height = int(np.ceil(image.shape[0] * cos + image.shape[1] * sin))
+
+    # Décalage pour recentrer l'image dans le nouveau canevas
+    R[0, 2] += new_width / 2 - center[0]
+    R[1, 2] += new_height / 2 - center[1]
+
     image = cv2.warpAffine(
-        image, R, (image.shape[1], image.shape[0]),
-        borderMode=cv2.BORDER_REFLECT,
+        image, R, #(image.shape[1], image.shape[0]),
+        (new_width, new_height),
+        borderMode=cv2.BORDER_REPLICATE,
     )
     #print("Rotation :", angle)
+    return image
+
+def center_crop_screw(image, dimension_shape):
+    contour = get_screw_contour(image)
+
+    [x,y,wr,hr] = cv2.boundingRect(contour)
+
+    h, w = dimension_shape[:2]
+    translation_x = w//2 - (x + wr//2)
+    translation_y = h//2 - (y + hr//2)
+
+    matrix = np.float32([
+        [1, 0, translation_x],
+        [0, 1, translation_y]
+    ])
+
+    image = cv2.warpAffine(
+        image,
+        matrix,
+        (image.shape[1], image.shape[0]),
+        borderMode=cv2.BORDER_REFLECT,
+    )
+
+    image = image[:h, :w]
+
     return image
 
 def measure_max_height(image):
@@ -52,18 +100,16 @@ def detect_head_side(image):
         return 1 # right
 
 def preprocess_screw(image):
-    image = image.astype(np.float32)
+    image = image.astype(np.uint8)
+    initial_shape = image.shape
 
-    contour = get_screw_contour(image)
-
-    [vx, vy, x0, y0] = cv2.fitLine(contour, cv2.DIST_L2,0,0.01,0.01)
-    angle = np.degrees(np.arctan2(vy, vx))[0]
-
-    image= rotateImage(image,angle)
+    image = align_horizontally(image)
 
     side = detect_head_side(image)
     if side > 0:
-        image=rotateImage(image, 180)
+        image=rotate_image(image, 180)
+
+    image = center_crop_screw(image, initial_shape)
     
     return image
 
@@ -71,7 +117,7 @@ def preprocess_screw_tf(image):
     processed_image = tf.numpy_function(
         func=preprocess_screw,
         inp=[image],
-        Tout=tf.float32,
+        Tout=tf.uint8,
     )
 
     # numpy_function fait perdre l'information de shape
@@ -83,29 +129,29 @@ def preprocess_screw_batch(image_batch):
     return tf.map_fn(
         preprocess_screw_tf,
         image_batch,
-        fn_output_signature=tf.float32,
+        fn_output_signature=tf.uint8,
     )
 
-"""
-image_number=np.random.randint(0,319)
-print(f"{image_number:03d}.png")
 
-image_path = Path("/home/win100/Documents/projet_liora/mvtec_anomaly_detection/screw/train/good/")
-image = cv2.imread(
-    image_path / f"{image_number:03d}.png"
-)
+def __main__():
+    image_number=np.random.randint(0,319)
+    image_number = 114
+    print(f"{image_number:03d}.png")
 
-image_modified = preprocess_screw(image)
+    image_path = Path("/home/win100/Documents/projet_liora/mvtec_anomaly_detection/screw/train/good/")
+    image = cv2.imread(
+        image_path / f"{image_number:03d}.png"
+    )
 
-plt.imshow(image_modified)
+    image_modified = preprocess_screw(image)
 
-# Dessin du contour
-# plt.plot(
-#     contour[:, 0, 0],
-#     contour[:, 0, 1],
-#     label="Contour"
-)
+    plt.imshow(image_modified)
+    # Dessin du contour
+    # plt.plot(
+    #     contour[:, 0, 0],
+    #     contour[:, 0, 1],
+    #     label="Contour"
+    # )
 
-plt.axis('off')
-plt.show();
-"""
+    plt.axis('off')
+    plt.show();
